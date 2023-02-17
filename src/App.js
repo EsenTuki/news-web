2fe7a764661ea819d9a7d19a2b989cf3a10f48a5
EsenTuki <56817562+EsenTuki@users.noreply.github.com>
2023-02-17 16:23:16 +0600

    Added a reducer to manage the state

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import News from './components/news/News';
 import Publication from './components/publication/Publication'
 import logo from './logo.svg';
 import { Route, Routes, BrowserRouter, NavLink } from 'react-router-dom';
+import { useReducer } from 'react';
+import { ContextApp, initialState, newsReducer } from './reducers/newsReducer';
 
 function App() {
+  const [state, dispatch] = useReducer(newsReducer, initialState)
   return (
     <BrowserRouter>
+
       <div>
         <div className='header'>
           <div className='header-item'>
@@ -15,10 +19,12 @@ function App() {
             </NavLink>
           </div>
         </div>
-        <Routes>
-          <Route path='/' element={<News />} />
-          <Route path='/publication/:id' element={<Publication />} />
-        </Routes>
+        <ContextApp.Provider value={{state, dispatch}}>
+          <Routes>
+            <Route path='/' element={<News />} />
+            <Route path='/publication/:id' element={<Publication />} />
+          </Routes>
+        </ContextApp.Provider>
       </div>
     </BrowserRouter>
   );
